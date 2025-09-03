c3c144fb9c04842649f4e8d07080b7c9af05fd09
s.maksimyuk <m4ximyuk@yandex.ru>
2025-09-03 20:32:46 +0300

    Sprint 8: Third iteration checkstyle fixes Epic

--- src/tracker/model/Epic.java
+++ src/tracker/model/Epic.java
@@ -2,7 +2,6 @@ package tracker.model;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
